659cb32406ab5cdedf7ced9fe0740a365e3861a3
Renan Oliveira <oliveira.renan.rg@gmail.com>
2020-11-18 15:31:18 -0300

    Exercícios 1 a 6 da pre aula 13.1

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Users from './Users';
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Route exact path='/' component={Home} />
+        <Route path='/about' component={About} />
+        <Route path='/users' component={Users} />  
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;

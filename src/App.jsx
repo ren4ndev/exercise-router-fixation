d8376ed38a72e64c1f482f277376f91e4993c9e0
Renan Oliveira <oliveira.renan.rg@gmail.com>
2020-11-18 16:03:28 -0300

    Terminando exercícios pré aula blobo 13.1 React Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 import Users from './Users';
+import StrictAccess from './StrictAccess'
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Route exact path='/' component={Home} />
-        <Route path='/about' component={About} />
-        <Route path='/users' component={Users} />  
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/users/:id' render={(props) => <Users {...props} owner="Renan" /> } />
+          <Route path='/strict-access' render={(props) => <StrictAccess {...props} username='joao' password='1234' />} />
+        </Switch>
+        <Link to='/strict-access' >Strict Access</Link>
       </BrowserRouter>
     );
   }
